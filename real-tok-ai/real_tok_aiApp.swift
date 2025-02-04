26e146f099e12fcd0cf385151c80702808c4b67c
lamardealmaker <41444155+lamardealmaker@users.noreply.github.com>
2025-02-03 18:04:52 -0600

    Initial Commit

---
+++ real-tok-ai/real_tok_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_tok_aiApp.swift
+//  real-tok-ai
+//
+//  Created by Learn on 3/02/25.
+//
+
+import SwiftUI
+
+@main
+struct real_tok_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
